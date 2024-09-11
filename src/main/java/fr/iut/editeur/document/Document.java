7e7acee0733c00cbabc4b7bee1bc34c070bbe2f0
Lyfoung <nicolas.lyfoung@etu.umontpellier.fr>
2024-09-11 09:04:28 +0200

    q1

---
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -0,0 +1,46 @@
+package fr.iut.editeur.document;
+
+public class Document {
+
+    private String texte;
+
+    public Document() {
+        this.texte = "";
+    }
+	
+    public String getTexte() {
+        return texte;
+    }
+
+    public void setTexte(String texte) {
+        this.texte = texte;
+    }
+
+    public void ajouter(String texte) {
+        this.texte += texte;
+    }
+
+    public void remplacer(int debut, int fin, String remplacement) {
+        String partieGauche = texte.substring(0, debut);
+        String partieDroite = texte.substring(fin + 1);
+        texte = partieGauche + remplacement + partieDroite;
+    }
+
+    public void majuscules(int debut, int fin) {
+        String maj = this.texte.substring(debut, fin + 1).toUpperCase();
+        remplacer(debut, fin, maj);
+    }
+
+    public void effacer(int debut, int fin) {
+        remplacer(debut, fin , "");
+    }
+
+    public void clear() {
+        texte = "";
+    }
+
+    @Override
+    public String toString() {
+        return this.texte;
+    }
+}
